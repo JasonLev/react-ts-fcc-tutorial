1f454200ce97e86ce1a9c785dd77c180efb80a3b
JasonLev <jasond.levy@gmail.com>
2023-07-28 14:38:09 -0700

    add TodoItem component

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -10,6 +10,24 @@ import { toast } from 'react-hot-toast'
 import cn from 'classnames'
 import { motion } from 'framer-motion'
 
-export const TodoItem = (props: { todo: Todo }) => {
-  return <div>Todo Item</div>
+export const TodoItem = ({status, text}: Todo ) => {
+
+  return (
+    <motion.li
+      layout
+      className={cn(
+        'p-5 rounded-xl bg-zinc-900',
+        status === 'completed' && 'bg-opacity-50 text-zinc-500',
+      )}
+    >
+      <motion.span
+        layout
+        style={{
+          textDecoration: status === 'completed' ? 'line-through' : 'none',
+        }}
+      >
+        {text}
+      </motion.span>
+    </motion.li>
+  )
 }
