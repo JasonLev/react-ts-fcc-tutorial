c51a05129af651bb60ca43943277d8114d811a91
JasonLev <jasond.levy@gmail.com>
2023-07-28 13:54:30 -0700

    add context, custom hook, implement hook in AddTodo

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -2,18 +2,34 @@ import React, { createContext, useState } from 'react'
 import { nanoid } from 'nanoid'
 import { useLocalStorage } from 'usehooks-ts'
 
+interface TodoContextProps {
+  todos: string[]
+  addTodo: (text: string) => void
+}
 export interface Todo {
   id: string
   text: string
   status: 'undone' | 'completed'
 }
 
-export const TodoContext = createContext<undefined>(undefined)
+export const TodoContext = createContext<TodoContextProps | undefined>(
+  undefined,
+)
 
 export const TodoProvider = (props: { children: React.ReactNode }) => {
+  const [todos, setTodos] = useState<string[]>([])
+
+  // ::: ADD NEW TODO :::
+  const addTodo = (text: string) => {
+    setTodos([...todos, text])
+  }
+
+  const value: TodoContextProps = {
+    todos,
+    addTodo,
+  }
+
   return (
-    <TodoContext.Provider value={undefined}>
-      {props.children}
-    </TodoContext.Provider>
+    <TodoContext.Provider value={value}>{props.children}</TodoContext.Provider>
   )
 }
